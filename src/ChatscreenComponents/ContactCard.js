16e8067145ea5f9ff824de4282acc837714baa1f
Elad Vizenblit <eladelad7@gmail.com>
2022-04-28 16:49:36 +0300

    Final Project

--- src/ChatscreenComponents/ContactCard.js
+++ src/ChatscreenComponents/ContactCard.js
@@ -65,16 +65,16 @@ const ContactCard = (props) => {
         <ul className="list-unstyled chat-list overflow-auto h-100">
             {
                 friendsObjects.map((friend) => (
-                    <div onClick={() => { props.setFriendChat(friend) }}>
+                    <div onClick={() => { props.setFriendChat(friend) }} id="clicker">
                         <li id="wrapper">
                             <img src={friend.avatar} />
                             <div id="#wrapper-2">
-                                <div>
-                                    <div className="name">{friend.nickname}</div>
-                                    <div id="latestComment" > {lastMessages(friend)} </div>
+                                <div id="wrapper-3">
+                                    <span className="name">{friend.nickname}</span>
+                                    <span id="time"> {timeago(friend)} </span>
                                 </div>
+                                <div id="latestComment" > {lastMessages(friend)} </div>
                             </div>
-                            <div id="time"> {timeago(friend)} </div>
                         </li>
                     </div>
                 ))
